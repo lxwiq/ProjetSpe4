6fa100ae65dbd16f486cb86fcbceb8ae22bd004b
lowiq <loic.aviez91@gmail.com>
2025-05-12 15:56:27 +0200

    Ajout du service d'authentification et intégration de la logique de connexion à la page de connexion dans le frontend

--- frontend/src/app/features/signin-page/signin-page.component.ts
+++ frontend/src/app/features/signin-page/signin-page.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ButtonComponent} from '../../core/components/button/button.component';
+import {AuthService} from '../../core/services/auth.service';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-signin-page',
   imports: [
-    ButtonComponent
+    ButtonComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './signin-page.component.html',
   styleUrl: './signin-page.component.css'
 })
-export class SigninPageComponent {
+export class SigninPageComponent implements OnInit {
+  mail = new FormControl('');
+  password = new FormControl('');
 
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.authService.login(this.mail.value || '', this.password.value || '');
+    if (this.authService.isLoggedIn()) {
+      window.location.href = '/dashboard';
+    }
+  }
+
+  onSubmit() {
+    this.authService.login(this.mail.value || '', this.password.value || '');
+    if (this.authService.isLoggedIn()) {
+      window.location.href = '/dashboard';
+    } else {
+      alert('Identifiants incorrects');
+    }
+  }
 }
