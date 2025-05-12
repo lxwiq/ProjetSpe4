3e5fd01a7f1f4d67cd6c9f9a72c989f60c3dccb6
lowiq <loic.aviez91@gmail.com>
2025-05-12 13:56:11 +0200

    Ajout du dashboard
    configuration des routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {SigninPageComponent} from './features/signin-page/signin-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: SigninPageComponent
+  }
+];
---
+++ frontend/src/app/features/signin-page/signin-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {ButtonComponent} from '../../core/components/button/button.component';
+
+@Component({
+  selector: 'app-signin-page',
+  imports: [
+    ButtonComponent
+  ],
+  templateUrl: './signin-page.component.html',
+  styleUrl: './signin-page.component.css'
+})
+export class SigninPageComponent {
+
+}
